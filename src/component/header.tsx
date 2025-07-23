48db4d42f0a238782994c9a7b09143eb21162edc
Aimeabibis <aimeabibis@gmail.com>
2025-07-24 00:44:18 +0200

    header

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -1,18 +1,21 @@
 export const Header = () => {
     return (
         <>
-        <header className="w-full h-20 flex justify-between items-center text-[#1a1a1a] bg-[#f1f1f1] shadow-md ">
+        <header className="w-full h-15 flex justify-between items-center text-[#1a1a1a] bg-[#f1f1f1] px-20 shadow-md ">
             <img src="" alt="" />
             <div>
-                <li className="flex space-x-4 text-lg font-semibold">
-                    <a href="/">Home</a>
-                    <a href="/about">About</a>
-                    <a href="/contact">Contact</a>
-                    <a href="/projects">Projects</a>
-                    <a href="/gallery">Gallery</a>
-                    <a href="/services">Services</a>
+                <li className="flex space-x-4 gap-4 ">
+                    <a href="/">Accueil</a>
+                    <a href="/about">Services</a>
+                    <a href="/contact">Produits</a>
+                    {/* <a href="/projects">Réalisation</a> */}
+                    <a href="/gallery">A propos</a>
+                    <a href="/services">Contact</a>
                 </li>
             </div>
+            <button>
+                Ajouter aux paniers
+            </button>
         </header>
         </>
     );
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,5 @@
 
-import { Header } from '../components/Header';
+import { Header } from '../component/header';
 
 export const Home = () => {
     return (<>
